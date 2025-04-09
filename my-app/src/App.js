fc4bcfb828e32f0065d5b10778c7f2ad30467139
alishuin <alison8gu9@gmail.com>
2025-04-08 18:31:31 -0700

    website yay

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,23 @@
 import {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {useDropzone" from 'react-dropzone';}
+/*import axios from 'axios';
 
 console.log("App component loaded");
+      <button onClick={this.fileUploadHandler}>Upload</button>
+class App extends Component{
+  state = {
+    selectedFile: null
+  }
 
+  fileSelectedHandler = event => {
+    console.log(event.target.files(0));
+  }
+}
+fileUploadHandler = () => {
+  axios.post('')
+}*/
 function App() {
 const [display, setDisplay] = useState(0);
 function addNum(){
@@ -11,17 +26,26 @@ function addNum(){
 function subNum(){
   setDisplay(display-1);
 }
+
+useDrop({
+  accept: {
+    'image/png': ['.jpg']
+    'text/html':['.html','.htm']
+  }
+})
   return (
     <div className = 'app'>
       <div className = 'add'>
         <button onClick = {addNum}>+</button>
       </div>
       <h1 className = 'header'>DataDrip!</h1>
-
-      <div className = 'subtract'>
-        <button onClick = {subNum}>-</button>
+      <h1> Welcome to DataDrip!</h1>
+      
+      
+      <h2 className = 'subheader'> Please insert a .jpg photo of your face in natural lighting from shoulders up</h2>
+      <input type="file"></input>
+            <button onClick>Upload</button>
       </div>
-    </div>
 
   );
 }
