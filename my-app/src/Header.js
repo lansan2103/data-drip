fd48cfe964587b02b2abae99a76953395846e428
alishuin <alison8gu9@gmail.com>
2025-04-17 00:14:03 -0700

    PAGES AND HEADER WORKS YAYAYAYAY

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 
 function Header() {
     return (
         <header className = 'header'>
-            <img src="logo.png" alt="DataDrip" className="logo" />
+            <img src="logo.png" alt="DataDrip" className="logo"/>
+            <Link to='/'>DataDrip</Link> 
+                {/* until i find out how to be able to click the DataDrip pic */}
+                
             <nav>
                 <ul>
-                    <a href="/">Take the Test</a>
-                    <a href="/about">About</a>
+                        <Link to='/page1'>Take the Test</Link>
+                    <br />
+                        <Link to='/page2'>About</Link>
                 </ul>
             </nav>
         </header>
     );
 }
+
 
 export default Header;
