c7c9b89a8ec9eadb9667e6a62b75497c2df60415
Nini Yen <yensydney@gmail.com>
2025-04-23 17:11:26 -0700

    fixed ip issue

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -61,7 +61,7 @@ function App() {
     const formData = new FormData();
     formData.append('file', file);
 
-    const response = await fetch('http://localhost:5000/upload', {
+    const response = await fetch('http://127.0.0.1:5000/upload', {
       method: 'POST',
       body: formData
     });
